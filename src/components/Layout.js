9e4ff89a2b2f7e8b255d125b9015597437064408
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-07-29 23:54:15 -0500

    Nprogress completed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,67 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
+import Nprogress from 'nprogress';
+import nProgress from 'nprogress';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
 
-const Layout = ({ children }) => {
+const Layout = ({ children, footer = true }) => {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    // Efecto para manejar cambios de ruta
+    const handleRouteChange = (url) => {
+      Nprogress.start();
+    };
+
+    const handleRouteComplete = (url) => {
+      Nprogress.done();
+    };
+
+    const handleRouteError = (url) => {
+      Nprogress.done();
+    };
+    
+    // Escucha el inicio de cambio de ruta
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    // Escucha el final exitoso de cambio de ruta
+    router.events.on("routeChangeComplete", handleRouteComplete);
+
+    // Escucha el error en el cambio de ruta
+    router.events.on("routeChangeError", handleRouteError);
+
+    return () => {
+      // Limpia el event listener al desmontar el componente
+      router.events.off("routeChangeStart", handleRouteChange);
+      router.events.off("routeChangeComplete", handleRouteComplete);
+      router.events.off("routeChangeError", handleRouteError);
+    };
+  }, []);
+
   return (
     <>
       <Navbar />
 
       {children}
+
+      { footer && (
+        <footer className="bg-dark text-light text-center">
+        <div className="container p-4">
+          <h1>&copy; Johan Eduardo Cano Ruiz Portfolio</h1>
+          <p>2024 - {new Date().getFullYear()}</p>
+          <p>All rights Reserved.</p>
+        </div>
+      </footer>
+      )}
     </>
   )
 }
 
-export default Layout
+Layout.propTypes = {
+  children: PropTypes.node,
+  footer: PropTypes.bool
+};
+
+export default Layout;
